ea2003315f35f2fe5e54142ca7a5cfe7e7b5822d
sothouth <55689222+sothouth@users.noreply.github.com>
2025-03-20 01:34:52 +0800

    feat: bevy cellular-automata

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin}, prelude::*, window::PresentMode};
 
 mod camera;
 mod input;
@@ -12,10 +12,13 @@ fn main() {
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 title: "Cellular Automata".to_string(),
+                present_mode: PresentMode::AutoNoVsync,
                 ..default()
             }),
             ..default()
         }))
+        .add_plugins(LogDiagnosticsPlugin::default())
+        .add_plugins(FrameTimeDiagnosticsPlugin)
         .add_plugins(input::InputPlugin)
         .add_plugins(cell::CellPlugin)
         .add_plugins(camera::CameraPlugin)
